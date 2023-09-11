8e6697ad2163cbc2e759e1971b64091d0d85efa1
Luis <luisflores1999@gmail.com>
2023-09-11 12:29:36 -0600

    nueva api Productos consulta para instalar

---
+++ backend/src_2/app.ts
@@ -0,0 +1,18 @@
+// servidor 
+import express from 'express';  // importamos express
+import morgan from 'morgan';    // importamos morgan
+const app = express(); // instanciamos express
+import indexRoutes from './routes/index'; // importamos indexRoutes      
+import path from 'path'; // importamos path
+// configuracion
+app.set('port', process.env.PORT|| 4000); // puerto
+// middlewares
+app.use(morgan('dev1')); // morgan 
+app.use(express.json()); // para que express entienda json  
+
+// routes
+app.use('/api',indexRoutes); // rutas 
+app.use('/uploads', express.static(path.resolve('uploads'))); // para que se pueda acceder a la carpeta uploads desde el navegador      
+// exportamos app
+export default app; 
+ 
---
+++ backend/src_2/models/Photo.ts
@@ -0,0 +1,14 @@
+import { Schema, model, Document } from "mongoose";
+const schema =new Schema({
+    title: String,
+    description: String,
+    imagePath: String
+
+}) ;
+
+interface IPhoto extends Document {
+    title: String;
+    description: String;
+    imagePath: String;
+}
+export default model<IPhoto>('Photo', schema);
